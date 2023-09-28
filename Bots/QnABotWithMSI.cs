74e4cf2f1c4b4918b1eee76ed1bc3bc7dc6b8972
Dacili <diname_girl@hotmail.com>
2023-09-28 11:04:57 +0200

    update

--- Bots/QnABotWithMSI.cs
+++ Bots/QnABotWithMSI.cs
@@ -18,7 +18,7 @@ namespace Microsoft.BotBuilderSamples.Bots
         protected readonly BotState ConversationState;
         protected readonly Microsoft.Bot.Builder.Dialogs.Dialog Dialog;
         protected readonly BotState UserState;
-        protected string defaultWelcome = "Hello and Welcome";
+        protected string defaultWelcome = "Welcome to FAQ Medi chatbot";
 
         public QnABotWithMSI(IConfiguration configuration, ConversationState conversationState, UserState userState, T dialog)
         {
